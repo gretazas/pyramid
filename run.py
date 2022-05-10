9c4ea61ff486acb179a58cbe66cf6bfc0ff21616
gretazas <esminis@yahoo.com>
2022-05-10 10:23:04 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import numpy as np
+
+# Display board and score board
+
+score_board = {'you': ['|', 'score: 0', '|'],
+               'bot': ['|', 'score: 0', '|']}
+
+board = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                  ['A', '', '', '', '', 'o', '', '', '', ''],
+                  ['B', '', '', '', 'o', '', 'o', '', '', ''],
+                  ['C', '', '', 'o', '', 'o', '', 'o', '', ''],
+                  ['D', '', 'o', '', 'o', '', 'o', '', 'o', ''],
+                  ['E', 'o', '', 'o', '', 'o', '', 'o', '', 'o']])
+
+game_is_on = True
+
+letters_to_numbers = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
+
+def print_board():
+    You = score_board['you']
+    print('Your score: ')
+    print(You)
+    Bot = score_board['bot']
+    print('Bot\'s score: ')
+    print(Bot)
+    print(board[0])
+    print(board[1])
+    print(board[2])
+    print(board[3])
+    print(board[4])
+    print(board[5])
+    
+
+print_board()
