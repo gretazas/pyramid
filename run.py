e6fd95299c70114e438b96a1d8ee1d43e2561817
gretazas <esminis@yahoo.com>
2022-06-03 16:22:33 +0000

    Print extra info when score and add more while GAME_IS_ON into game logic to avoid infinitive loops

--- run.py
+++ run.py
@@ -12,14 +12,20 @@ score_board = {'your_score': 0,
                'bots_score': 0}
 
 board = np.array([
- ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
- ['o', '', '', 'o', 1, 2, 3, 4, 5, 6, 7, 8, 9, '.', '.', '.', '.'],
+ ['', '', '', 'o', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+ ['o', '', '', '.', 1, 2, 3, 4, 5, 6, 7, 8, 9, '.', '.', '.', ''],
  ['', '', '', 'A', '', '', '', '', 'o', '', '', '', '', '', '', '', ''],
  ['', '', '', 'B', '', '', '', 'o', '', 'o', '', '', '', '', '', '', ''],
  ['', '', '', 'C', '', '', 'o', '', 'o', '', 'o', '', '', '', '', '', ''],
  ['', '', '', 'D', '', 'o', '', 'o', '', 'o', '', 'o', '', '', '', '', ''],
  ['', '', '', 'E', 'o', '', 'o', '', 'o', '', 'o', '', 'o', '', '', '', ''],
- ['', 'o', 'o', '', '', '', '', '', '', '', '', '', '', 'o', 'o', 'o', 'o']])
+ ['', 'o', 'o', '', '', '', '', '', '', '', '', '', '', '', 'o', 'o', 'o'],
+ ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+ ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+ ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+ ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+ ['', '', '', '', '', '', '', '', '', '', '', '', '', 'o', '', '', ''],
+ ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']])
 
 GAME_IS_ON = True
 ACTIVE_PLAYERS_SCORE = 'your_score'
@@ -67,38 +73,45 @@ def position_inputs():
 def position_is_valid(row, column):
     """Valid posiotion choises"""
 
-    if row not in range(1, 7):
-        print('Not a required letter, please start again')
-        position_inputs()
-    elif column not in range(4, 13):
-        print('Not a required number, please try again')
-        position_inputs()
-    elif board[row][column] == '@':
-        print('Already chosen, please try again.')
-        position_inputs()
-    elif board[row][column] == '':
-        print('Emply field, please try again.')
-        position_inputs()
-    else:
-        board[row][column] = '@'
-        if 'o' in board[2:7, 4:13]:
-            print(board[1:7, 3:13])
-            check_for_lines()
+    global GAME_IS_ON
+    if GAME_IS_ON:
+        if row not in range(1, 7):
+            print('Not a required letter, please start again')
+            position_inputs()
+        elif column not in range(4, 13):
+            print('Not a required number, please try again')
+            position_inputs()
+        elif board[row][column] == '@':
+            print('Already chosen, please try again.')
+            position_inputs()
+        elif board[row][column] == '':
+            print('Emply field, please try again.')
+            position_inputs()
         else:
-            decide_winner()
+            board[row][column] = '@'
+            if 'o' in board[2:7, 4:13]:
+                print(board[0:14, 0:17])
+                check_for_lines()
+            else:
+                GAME_IS_ON = False
+    else:
+        decide_winner()
 
 
 def while_rows():
     '''Get eatch row and get score for it'''
 
-    all_rows = board[1:6, :]
+    all_rows = board[1:7, :]
     for one_row in all_rows:
+        old_score = score_board.get(ACTIVE_PLAYERS_SCORE)
         while 'o' not in one_row:
             score_board[ACTIVE_PLAYERS_SCORE] += np.count_nonzero(one_row)
             score_board[ACTIVE_PLAYERS_SCORE] -= 1
             # it would not include letter at index 0
-            one_row[13] = 'o'
-            print('row score:', score_board[ACTIVE_PLAYERS_SCORE])
+            one_row[16] = 'o'
+            new_value = score_board.get(ACTIVE_PLAYERS_SCORE)
+            update = new_value - old_score
+            print('Line in row', ACTIVE_PLAYERS_SCORE, 'is up by', update)
             break
 
 
@@ -106,12 +119,15 @@ def while_columns():
     '''Get eatch column and get score for it'''
 
     for one_column in board.T:
+        old_score = score_board.get(ACTIVE_PLAYERS_SCORE)
         while 'o' not in one_column:
             score_board[ACTIVE_PLAYERS_SCORE] += np.count_nonzero(one_column)
             score_board[ACTIVE_PLAYERS_SCORE] -= 1
             # it would not include number at index 0
             one_column[len(one_column)-1] = 'o'
-            print('column score:', score_board[ACTIVE_PLAYERS_SCORE])
+            new_value = score_board.get(ACTIVE_PLAYERS_SCORE)
+            update = new_value - old_score
+            print('Line in column', ACTIVE_PLAYERS_SCORE, 'is up by', update)
             break
 
 
@@ -121,27 +137,43 @@ def while_diagonals():
     while 'o' not in board.diagonal():
         score_board[ACTIVE_PLAYERS_SCORE] += 2
         board[0][0] = 'o'
+        print('Line in diagonal', ACTIVE_PLAYERS_SCORE, 'is up by 2')
+        break
     while 'o' not in board.diagonal(2):
         score_board[ACTIVE_PLAYERS_SCORE] += 3
         board[0][2] = 'o'
+        print('Line in diagonal', ACTIVE_PLAYERS_SCORE, 'is up by 3')
+        break
     while 'o' not in board.diagonal(4):
         score_board[ACTIVE_PLAYERS_SCORE] += 4
-        board[0][4] = 'o'
+        board[12][16] = 'o'
+        print('Line in diagonal', ACTIVE_PLAYERS_SCORE, 'is up by 4')
+        break
     while 'o' not in board.diagonal(6):
         score_board[ACTIVE_PLAYERS_SCORE] += 5
-        board[0][6] = 'o'
-    while 'o' not in np.flipud(board).diagonal(3):
+        board[10][16] = 'o'
+        print('Line in diagonal', ACTIVE_PLAYERS_SCORE, 'is up by 5')
+        break
+    while 'o' not in np.flipud(board).diagonal(-3):
         score_board[ACTIVE_PLAYERS_SCORE] += 5
-        board[0][10] = 'o'
-    while 'o' not in np.flipud(board).diagonal(5):
+        board[10][0] = 'o'
+        print('Line in diagonal', ACTIVE_PLAYERS_SCORE, 'is up by 5')
+        break
+    while 'o' not in np.flipud(board).diagonal(-1):
         score_board[ACTIVE_PLAYERS_SCORE] += 4
-        board[0][12] = 'o'
-    while 'o' not in np.flipud(board).diagonal(7):
+        board[12][0] = 'o'
+        print('Line in diagonal', ACTIVE_PLAYERS_SCORE, 'is up by 4')
+        break
+    while 'o' not in np.flipud(board).diagonal(1):
         score_board[ACTIVE_PLAYERS_SCORE] += 3
         board[0][14] = 'o'
-    while 'o' not in np.flipud(board).diagonal(9):
+        print('Line in diagonal', ACTIVE_PLAYERS_SCORE, 'is up by 3')
+        break
+    while 'o' not in np.flipud(board).diagonal(3):
         score_board[ACTIVE_PLAYERS_SCORE] += 2
         board[0][16] = 'o'
+        print('Line in diagonal', ACTIVE_PLAYERS_SCORE, 'is up by 2')
+        break
 
 
 def check_for_lines():
@@ -165,23 +197,25 @@ def check_for_lines():
 def change_active_player():
     '''Change active player'''
 
-    global ACTIVE_PLAYERS_SCORE
-    if ACTIVE_PLAYERS_SCORE == 'your_score':
-        ACTIVE_PLAYERS_SCORE = 'bots_score'
-        print('bots score=', ACTIVE_PLAYERS_SCORE)
-        bot_position_choose()
-        return ACTIVE_PLAYERS_SCORE
+    global GAME_IS_ON
+    if GAME_IS_ON:
+        global ACTIVE_PLAYERS_SCORE
+        if ACTIVE_PLAYERS_SCORE == 'your_score':
+            ACTIVE_PLAYERS_SCORE = 'bots_score'
+            bot_position_choose()
+            return ACTIVE_PLAYERS_SCORE
+        else:
+            ACTIVE_PLAYERS_SCORE = 'your_score'
+            position_inputs()
     else:
-        ACTIVE_PLAYERS_SCORE = 'your_score'
-        print('your score=', ACTIVE_PLAYERS_SCORE)
-        position_inputs()
+        position_is_valid(0, 0)
 
 
 def bot_position_choose():
     '''Make computer to choose a random position in board'''
-    print('got to change_active_player1')
-    bots_position_in_row = random.randint(2, 7)
-    bots_position_in_column = random.randint(4, 13)
+
+    bots_position_in_row = random.randint(2, 6)
+    bots_position_in_column = random.randint(4, 12)
     # I need to eliminate emplty spaces in order to choose right position
     if board[bots_position_in_row][bots_position_in_column] != 'o':
         bot_position_choose()
@@ -189,7 +223,7 @@ def bot_position_choose():
         # Once got 'o' in board mark it '@' as taken
         board[bots_position_in_row][bots_position_in_column] = '@'
 
-    print(board[1:7, 3:13])
+    print(board[0:14, 0:17])
     # Check if scored:
     check_for_lines()
 
@@ -198,11 +232,17 @@ def decide_winner():
     '''Decide winner'''
 
     if score_board['your_score'] > score_board['bots_score']:
+        print('/////////////////////////////////////////')
         print('Congradulations! You are the winner!')
+        print('/////////////////////////////////////////')
     elif score_board['your_score'] < score_board['bots_score']:
+        print('/////////////////////////////////////////')
         print('You lost! Sorry.')
+        print('/////////////////////////////////////////')
     else:
+        print('/////////////////////////////////////////')
         print('It\'s a draw.')
+        print('/////////////////////////////////////////')
 
 
 init_game()
